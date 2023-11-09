288529fd14d2a7013cfd371b0670e80d34ae7450
Bodzio0019 <nikolas.ed@onet.eu>
2023-11-09 21:57:38 +0100

    Dodano główną część kodu w JS

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello");
+const colors = ["white", "blue", "green", "yellow", "gold", "gray"];
+const but = document.querySelector("button");
+
+function change() {
+    let changed = document.body.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+    document.querySelector(".color").innerHTML = changed.toUpperCase();
+};
+
+but.addEventListener("click", change);
