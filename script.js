ca4612517e4e464cb595f703483e1c412558fad9
Bodzio0019 <nikolas.ed@onet.eu>
2023-11-20 07:08:09 +0100

    Dodano funkcję, która eliminuje powtórki, ale zmieniono wyświetlanie nazwy koloru na RGB

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const colors = ["white", "blue", "green", "yellow", "gold", "gray"];
+const colors = ["rgb(255, 255, 255)", "rgb(0, 0, 255)", "rgb(0, 128, 0)", "rgb(255, 255, 0)", "rgb(255, 215, 0)", "rgb(128, 128, 128)"];
 const but = document.querySelector("button");
+const body = document.body;
 
 function change() {
-    let changed = document.body.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
-    document.querySelector(".color").innerHTML = changed.toUpperCase();
+    let rand = colors[Math.floor(Math.random() * colors.length)];
+    let current = getComputedStyle(body).backgroundColor;
+    if(rand === current) {
+        change();
+        console.log("HI");
+    } else{
+        console.log("NO", current);
+        let changed = document.body.style.backgroundColor = rand;
+        document.querySelector(".color").innerHTML = changed.toUpperCase();
+    }
 };
+
 
 but.addEventListener("click", change);
