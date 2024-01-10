5e9c56409ceeceb80a51f3935e41cb7c8e98db2d
Bodzio0019 <nikolas.ed@onet.eu>
2024-01-10 12:48:34 +0100

    Zmieniono pokazywanie koloru z RGB na nazwę

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const colors = ["rgb(255, 255, 255)", "rgb(0, 0, 255)", "rgb(0, 128, 0)", "rgb(255, 255, 0)", "rgb(255, 215, 0)", "rgb(128, 128, 128)"];
+const names = ["white", "blue", "green", "yellow", "gold", "grey"];
 const but = document.querySelector("button");
 const body = document.body;
 
 function change() {
-    let rand = colors[Math.floor(Math.random() * colors.length)];
+    let num = Math.floor(Math.random() * colors.length);
+    let rand = colors[num];
     let current = getComputedStyle(body).backgroundColor;
     if(rand === current) {
         change();
         console.log("HI");
     } else{
-        console.log("NO", current);
+        console.log("NO", num);
         let changed = document.body.style.backgroundColor = rand;
-        document.querySelector(".color").innerHTML = changed.toUpperCase();
+        document.querySelector(".color").innerHTML = names[num].toUpperCase();
     }
 };
 
