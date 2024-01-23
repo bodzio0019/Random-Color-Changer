ea53eae9a5ad0ccdc83b8cfb0417ef6e49eba65e
Bodzio0019 <nikolas.ed@onet.eu>
2024-01-23 18:49:02 +0100

    Dodano nowy przycisk w HTML, CSS, i rozdzielono działanie przycisków w JS

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const colors = ["rgb(255, 255, 255)", "rgb(0, 0, 255)", "rgb(0, 128, 0)", "rgb(255, 255, 0)", "rgb(255, 215, 0)", "rgb(128, 128, 128)"];
 const names = ["white", "blue", "green", "yellow", "gold", "grey"];
-const but = document.querySelector("button");
+const but = document.querySelector(".button1");
 const body = document.body;
 
 function change() {
